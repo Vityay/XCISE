0e714aeae15b29c5cea8ebb413c20b7529e44ca4
Victor Guryev <victor.guryev@gmail.com>
2024-05-08 13:02:27 +0200

    Add files via upload

---
+++ vcf4wasp.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $snv_file = shift or die "Usage: perl $0 <variant_file>\n";
+die "Input file is not readable" unless -r $snv_file;
+
+open( F, $snv_file ) or die 'Cannot open infile';
+while ( <F> ) {
+    if ( m/^\#/ ) {
+        print;
+        next;
+    }
+    chomp;
+    my ( $chr, $pos, $rs, $ref, $alt, $varq, $filter, $info, $format ) = split /\t/;
+    next unless $ref =~ m/^[GATC]$/ and $alt =~ m/^[GATC]$/; #Only SNVs
+    my ($ac) = $info =~ m/AC\=(\d+)/;
+    my ($an) = $info =~ m/AN\=(\d+)/;
+    my ($dp4) = $info =~ m/DP4\=([\d\,]+)/;
+    my ( $ref_pos, $ref_neg, $alt_pos, $alt_neg ) = split /\,/, $dp4;
+    my $total = $ref_pos + $ref_neg + $alt_pos + $alt_neg;
+    my $af = ( $alt_pos+$alt_neg) / $total; 
+    next if $af < 0.1 or $af > 0.9;
+    print join( "\t", $chr, $pos, $rs, $ref, $alt, '.', '.', '.', 'GT', '0|1' ), "\n";
+}
+close F;
+
